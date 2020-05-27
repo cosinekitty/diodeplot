a0a02f7171674c2774c6fea3edf4ff718807f27f
Don Cross <cosinekitty@gmail.com>
2020-05-27 19:49:13 -0400

    Cleaned up code and added more comments.

--- DiodePlotFirmware/DiodePlotFirmware.ino
+++ DiodePlotFirmware/DiodePlotFirmware.ino
@@ -1,29 +1,35 @@
 /*
     DiodePlotFirmware.ino  -  Don Cross  -  2020-05-27
 
+    https://github.com/cosinekitty/diodeplot
+
     Arduino Uno firmware for measuring current/voltage
     relationship for a diode.
 */
 
-#define BINARY(a,b,c,d,e,f,g,h) ((a)<<7|(b)<<6|(c)<<5|(d)<<4|(e)<<3|(f)<<2|(g)<<1|(h))
-
-
 void setup()
 {
-    PIND   = BINARY(0,0,0,0,0,0,0,0);       // disable all pullup resistors on D register
-    PORTD  = BINARY(0,0,0,0,0,0,0,0);       // turn off all output pins on D register
-    DDRD   = BINARY(1,1,1,1,1,1,1,1);       // all D register pins are outputs
+    PIND   = 0x00;       // disable all pullup resistors on D register
+    PORTD  = 0x00;       // turn off all output pins on D register
+    DDRD   = 0xff;       // all D register pins are outputs
 
     Serial.begin(115200);
+
+    delay(5000);        // wait 5 seconds before starting the test
 }
 
 void loop()
 {
+    // We cycle through all possible combination of 8 binary outputs:
+    // 0 through 255, to produce 256 possible voltage levels.
     ++PORTD;
-    delay(50);
+
+    delay(50);      // Wait a little while for voltage to stabilize.
+
     int v1 = analogRead(0);     // op-amp output voltage
     int v2 = analogRead(1);     // voltage on diode's anode
 
+    // Log the data.
     Serial.print((int)PORTD);
     Serial.print(" ");
     Serial.print(v1);
