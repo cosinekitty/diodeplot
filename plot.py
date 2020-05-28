2e53e17baa293b85e7535c265b78da2e60f23a20
Don Cross <cosinekitty@gmail.com>
2020-05-28 18:28:55 -0400

    Made plot.py more flexible: can select variables for x and y axes.

--- plot.py
+++ plot.py
@@ -17,10 +17,27 @@ R9 = 0.3272     # measured value of the resistor in series with the diode, in K 
 def Voltage(a):
     return 0.005 * a
 
-def PlotFile(filename):
+class Sample:
+    def __init__(self, n, a1, a2):
+        self.n = n      # The DPORT value: 0..255
+        self.a1 = a1    # The analog reading for V1 = voltage output from op-amp
+        self.a2 = a2    # The analog reading for V2 = voltage between resistor and test device
+
+    def GetVar(self, varname):
+        if varname == 'n':
+            return self.n
+        if varname == 'v1':
+            return Voltage(self.a1)
+        if varname == 'v2':
+            return Voltage(self.a2)
+        if varname == 'i':
+            return (Voltage(self.a1-self.a2)) / R9
+        raise Exception('Invalid variable name: ' + varname)
+
+
+def LoadData(filename):
     lnum = 0
-    vlist = []      # list of anode voltages in V
-    ilist = []      # list of currents in mA
+    data = []
     with open(filename, 'rt') as infile:
         for line in infile:
             line = line.strip()
@@ -29,23 +46,39 @@ def PlotFile(filename):
                 m = re.match(r'^(\d+)\s+(\d+)\s+(\d+)$', line)
                 if not m:
                     print('ERROR({} line {}): invalid data format'.format(filename, lnum))
-                    return 1
-                #n = int(m.group(1))
+                    sys.exit(1)
+                n = int(m.group(1))
                 a1 = int(m.group(2))
                 a2 = int(m.group(3))
-                v1 = Voltage(a1)
-                v2 = Voltage(a2)
-                i = (v1 - v2) / R9
-                vlist.append(v2)
-                ilist.append(i)
-    plt.plot(vlist, ilist, 'b.')
+                data.append(Sample(n, a1, a2))
+    return data
+
+
+def PlotFile(xvar, yvar, filename):
+    data = LoadData(filename)
+    xlist = []
+    ylist = []
+    for d in data:
+        xlist.append(d.GetVar(xvar))
+        ylist.append(d.GetVar(yvar))
+    plt.plot(xlist, ylist, 'b.')
     plt.show()
     plt.close('all')    # free all memory
     return 0
 
-if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        sys.exit(PlotFile(sys.argv[1]))
 
-    print('USAGE:  plot.py datafile.txt')
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        sys.exit(PlotFile(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    print(r'''
+USAGE:  plot.py x y datafile.txt
+
+Where x and y are each one of the following:
+
+    n  = the DPORT value 0..255 that is fed into the resistor network.
+    v1 = measured voltage coming out of the op-amp.
+    v2 = measured voltage fed into the test device.
+    i  = deduced current calculated as (v1-v2)/R.
+''')
     sys.exit(1)
