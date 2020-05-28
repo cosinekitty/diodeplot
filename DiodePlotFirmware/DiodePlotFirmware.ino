0402beef1c2faa48f5a92c66f1cbdbd15a107eba
Don Cross <cosinekitty@gmail.com>
2020-05-27 20:20:56 -0400

    Added serial command interface. Gather data faster.

--- DiodePlotFirmware/DiodePlotFirmware.ino
+++ DiodePlotFirmware/DiodePlotFirmware.ino
@@ -7,6 +7,17 @@
     relationship for a diode.
 */
 
+enum RunState
+{
+    WAITING,
+    RUNNING,
+};
+
+RunState state = WAITING;
+int trial = 0;
+int limit = 10;
+int accum;
+
 void setup()
 {
     PIND   = 0x00;       // disable all pullup resistors on D register
@@ -14,26 +25,91 @@ void setup()
     DDRD   = 0xff;       // all D register pins are outputs
 
     Serial.begin(115200);
-
-    delay(5000);        // wait 5 seconds before starting the test
+    delay(1000);
+    Serial.println("# READY");
 }
+
 
 void loop()
 {
-    // We cycle through all possible combination of 8 binary outputs:
-    // 0 through 255, to produce 256 possible voltage levels.
-    ++PORTD;
+    switch (state)
+    {
+    case WAITING:
+        break;      // do nothing
 
-    delay(50);      // Wait a little while for voltage to stabilize.
+    case RUNNING:
+        if (PORTD == 0)
+        {
+            // Should we start another trial run, or are we finished?
+            ++trial;
+            if (trial > limit)
+            {
+                // We have finished.
+                Serial.println("# FINISHED");
+                state = WAITING;
+                return;
+            }
 
-    int v1 = analogRead(0);     // op-amp output voltage
-    int v2 = analogRead(1);     // voltage on diode's anode
+            Serial.print("# trial ");
+            Serial.print(trial);
+            Serial.print(" of ");
+            Serial.print(limit);
+            Serial.println();
+        }
 
-    // Log the data.
-    Serial.print((int)PORTD);
-    Serial.print(" ");
-    Serial.print(v1);
-    Serial.print(" ");
-    Serial.print(v2);
-    Serial.println();
+        delay(1);      // Wait a little while for voltage to stabilize.
+
+        int v1 = analogRead(0);     // op-amp output voltage
+        int v2 = analogRead(1);     // voltage on diode's anode
+
+        // Log the data.
+        Serial.print((int)PORTD);
+        Serial.print(" ");
+        Serial.print(v1);
+        Serial.print(" ");
+        Serial.print(v2);
+        Serial.println();
+
+        // We cycle through all possible combination of 8 binary outputs on the D register:
+        // 0 through 255, to produce 256 possible voltage levels.
+        ++PORTD;
+        break;
+    }
+
+    while (Serial.available() > 0)
+    {
+        char c = Serial.read();
+        switch (c)
+        {
+        case 'z':       // reset and print accumulator
+            accum = 0;
+            // fall through to print
+        case 'a':       // print accumulator
+            Serial.print("# accum = ");
+            Serial.print(accum);
+            Serial.println();
+            break;
+
+        case 'r':       // run or restart
+            state = RUNNING;
+            trial = 0;
+            limit = accum;
+            break;
+
+        case 'h':       // halt
+            state = WAITING;
+            PORTD = 0;          // turn off the output voltage
+            Serial.println("# HALTED");
+            break;
+
+        case '?':       // confirm alive
+            Serial.println("# OK");
+            break;
+
+        default:
+            if (c >= '0' && c <= '9')
+                accum = (10 * accum) + (c - '0');
+                break;
+        }
+    }
 }
